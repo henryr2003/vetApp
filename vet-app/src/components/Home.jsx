eb4b45d2d73d4ec1d4ae6ee84d947e53e314cf1f
henryr2003 <henryrodas2003@gmail.com>
2025-09-09 13:31:41 -0700

    navBar styling and account tab animation

--- vet-app/src/components/Home.jsx
+++ vet-app/src/components/Home.jsx
@@ -10,7 +10,7 @@ import "../styles/Home.css"
 function Home() {
   const [pets, setPets] = useState([]);
   const [form, setForm] = useState({ name: '', type: '', age: '' });
-  
+  const [isTabOpen, setTab] = useState(false);
   
   const {user, logout } = useAuth();
     // const user = true;
@@ -84,9 +84,25 @@ const handleLogOut = async () => {
     }
   }
 
+function toggleTab() {
+  setTab(prev => !prev);
+  console.log("changing tab");
+  
+}
   return (
+    <>
 
-    <div className="mainContainer">
+    <div className={`account-tab ${isTabOpen ?  "open" : ""}`} > 
+          <div className='closeTab' onClick={toggleTab}>
+            X
+          </div>
+          {!user && <Link className="login" to="/login"> Login </Link> }
+
+          {user && <button onClick={() => {handleLogOut(); toggleTab()}}> Log Out </button>}
+          
+        </div>
+    
+    <div className="mainContainer" onClick={isTabOpen ? toggleTab : undefined} >
 
       <div className='header'> 
 
@@ -98,16 +114,18 @@ const handleLogOut = async () => {
         <img className="headerLogo" src= {amvcLogo} />
 
 
-        <svg className="account" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#2094A1"><path d="M234-276q51-39 114-61.5T480-360q69 0 132 22.5T726-276q35-41 54.5-93T800-480q0-133-93.5-226.5T480-800q-133 0-226.5 93.5T160-480q0 59 19.5 111t54.5 93Zm246-164q-59 0-99.5-40.5T340-580q0-59 40.5-99.5T480-720q59 0 99.5 40.5T620-580q0 59-40.5 99.5T480-440Zm0 360q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q53 0 100-15.5t86-44.5q-39-29-86-44.5T480-280q-53 0-100 15.5T294-220q39 29 86 44.5T480-160Zm0-360q26 0 43-17t17-43q0-26-17-43t-43-17q-26 0-43 17t-17 43q0 26 17 43t43 17Zm0-60Zm0 360Z"/></svg>
+        <svg onClick={toggleTab} className="account" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#2094A1"><path d="M234-276q51-39 114-61.5T480-360q69 0 132 22.5T726-276q35-41 54.5-93T800-480q0-133-93.5-226.5T480-800q-133 0-226.5 93.5T160-480q0 59 19.5 111t54.5 93Zm246-164q-59 0-99.5-40.5T340-580q0-59 40.5-99.5T480-720q59 0 99.5 40.5T620-580q0 59-40.5 99.5T480-440Zm0 360q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q53 0 100-15.5t86-44.5q-39-29-86-44.5T480-280q-53 0-100 15.5T294-220q39 29 86 44.5T480-160Zm0-360q26 0 43-17t17-43q0-26-17-43t-43-17q-26 0-43 17t-17 43q0 26 17 43t43 17Zm0-60Zm0 360Z"/></svg>
+
+        
       </div>
 
-      <h1>Vet App</h1>
+      <h1>Waitlist</h1>
 
-      {!user && <Link to="/login"> Login </Link> }
+      
       
       {user && <>
       
-        <button onClick={() => handleLogOut()}> Log Out </button>
+        
 
     <form onSubmit={handleSubmit}>
         <input placeholder="Name" value={form.name} onChange={e => setForm({ ...form, name: e.target.value })} />
@@ -137,6 +155,8 @@ const handleLogOut = async () => {
       </ul>
       
     </div>
+
+    </>
   );
 }
 
